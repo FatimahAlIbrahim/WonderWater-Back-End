81c49130fcb1f68e0219c753ecc49c65132d268b
Fatimah-Al-Ibrahim <Fatimah.Al.Ibrahim.1997@gmail.com>
2021-02-18 15:33:50 +0300

    created spring boot project

---
+++ wonderwater/src/main/java/com/ga/wonderwater/WonderwaterApplication.java
@@ -0,0 +1,13 @@
+package com.ga.wonderwater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderwaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderwaterApplication.class, args);
+	}
+
+}
---
+++ wonderwater/src/test/java/com/ga/wonderwater/WonderwaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.wonderwater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderwaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
