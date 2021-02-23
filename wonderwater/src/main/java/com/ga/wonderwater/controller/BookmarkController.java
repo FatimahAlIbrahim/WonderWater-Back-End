30d9fd8ca3118dd1bcfbb1864f781dc55b0a0889
Fatimah-Al-Ibrahim <Fatimah.Al.Ibrahim.1997@gmail.com>
2021-02-23 22:18:32 +0300

    added bookmark controller and checked its api with postman

---
+++ wonderwater/src/main/java/com/ga/wonderwater/controller/BookmarkController.java
@@ -0,0 +1,41 @@
+package com.ga.wonderwater.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ga.wonderwater.dao.BookmarkDao;
+import com.ga.wonderwater.model.Bookmark;
+import com.ga.wonderwater.model.User;
+
+@RestController
+public class BookmarkController {
+	
+	@Autowired
+	BookmarkDao bookmarkDao;
+	
+	@GetMapping("/bookmark/index")
+	public Iterable<Bookmark> bookmarkDetails(@RequestBody User user) {
+		var it = bookmarkDao.findByUser(user);
+		return it;
+	}
+	
+	@PostMapping("/bookmark/add")
+	public Bookmark addBookmark(@RequestBody Bookmark bookmark) {
+		bookmarkDao.save(bookmark);
+		return bookmark;
+	}
+	
+	@DeleteMapping("/bookmark/delete")
+	public boolean deleteBookmark(@RequestParam int id) {
+		bookmarkDao.deleteById(id);
+		return true;
+	}
+	
+	
+
+}
--- wonderwater/src/main/java/com/ga/wonderwater/dao/BookmarkDao.java
+++ wonderwater/src/main/java/com/ga/wonderwater/dao/BookmarkDao.java
@@ -2,7 +2,10 @@ package com.ga.wonderwater.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import com.ga.wonderwater.model.Bookmark;
+import com.ga.wonderwater.model.User;
 
 public interface BookmarkDao extends CrudRepository<Bookmark, Integer>{
+	
+	public Iterable<Bookmark> findByUser(User user);
 
 }
